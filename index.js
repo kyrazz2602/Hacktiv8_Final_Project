fe79024ac9a213ac690af9c4dd978c4a638d1c04
Arizal Anshori <130853401+Zalzz2602@users.noreply.github.com>
2025-08-16 13:01:52 +0700

    perbarui: perbaiki format kode di index.js dengan konsistensi penggunaan tanda kutip ganda dan penataan ulang kode untuk meningkatkan keterbacaan. Tambahkan penanganan kesalahan yang lebih baik pada endpoint chat.

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const app = express();
 // tambahkan middleware-nya ke dalam app
 app.use(cors()); // CORS: Cross-Origin Resource Sharing
 app.use(express.json()); // membolehkan request dengan Content-Type: application/json
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // The client gets the API key from the environment variable `GEMINI_API_KEY`.
 const ai = new GoogleGenAI({}); // instantiate sebuah class menjadi sebuah instance object di variable `ai`
@@ -24,7 +24,7 @@ const ai = new GoogleGenAI({}); // instantiate sebuah class menjadi sebuah insta
 // request untuk POST /chat
 // http://localhost:3000/chat --> POST
 // JSON body: { prompt: "" }
-app.post('/chat', async (req, res) => {
+app.post("/chat", async (req, res) => {
   // guard clause (satpam)
   if (!req.body) {
     // lempar 400
@@ -36,37 +36,39 @@ app.post('/chat', async (req, res) => {
 
   // kasih satpam lagi buat periksa isi prompt-nya
   if (!messages || !Array.isArray(messages)) {
-    return res.status(400).send(
-      "Tidak ada pesan nih, atau pesannya nggak beraturan! Pesan harus berupa array berisi role dan juga content"
-    );
+    return res
+      .status(400)
+      .send(
+        "Tidak ada pesan nih, atau pesannya nggak beraturan! Pesan harus berupa array berisi role dan juga content"
+      );
   }
 
   // Array mapping
   // looping untuk mengubah (mapping) dari satu object/tipe ke tipe/object lain
-  const contents = messages.map(message => {
+  const contents = messages.map((message) => {
     return {
       role: message.role,
-      parts: [
-        { text: message.content }
-      ]
-    }
-  })
+      parts: [{ text: message.content }],
+    };
+  });
 
   try {
     const aiResponse = await ai.models.generateContent({
       config: {
         systemInstruction: {
           parts: [
-            { text: "Anda adalah seorang seniman yang gemar membuat puisi sebagai jawaban." }
-          ]
-        }
+            {
+              text: "Anda adalah seorang seniman yang gemar membuat puisi sebagai jawaban.",
+            },
+          ],
+        },
       },
       model: "gemini-2.5-flash-lite",
-      contents // shorthand dari "contents: contents"
-    })
+      contents, // shorthand dari "contents: contents"
+    });
 
     return res.status(200).json({
-      response: aiResponse.text
+      response: aiResponse.text,
     });
   } catch (e) {
     return res.status(500).send(e.message);
